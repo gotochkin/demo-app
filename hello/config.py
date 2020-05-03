9410f37a9225697fa47fbfe7b01c606db5cd855c
gotochkin <otochkin@gleb-demo-01.publicsubnetffk.vcnglebus.oraclevcn.com>
2020-05-03 13:05:17 +0000

    fixes

--- hello/config.py
+++ hello/config.py
@@ -2,7 +2,8 @@ SECRET_KEY = 'CHANGE_ME'
 # database connection uri
 import cx_Oracle
 dsnStr = cx_Oracle.makedsn('gleb-bastion-us','1521','XEPDB1')
-SQLALCHEMY_DATABASE_URI = 'oracle://appdb:welcome1' + dsnStr.replace('SID', 'SERVICE_NAME')
+SQLALCHEMY_DATABASE_URI = 'oracle://appdb:welcome1@' + dsnStr.replace('SID', 'SERVICE_NAME')
 STATIC_ROOT = None
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
